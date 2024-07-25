cbc1ba5e97bd252c9fa2500979a76962beb6a132
Pablo Alimasi <pabloalimasi233@gmail.com>
2024-07-25 20:40:27 +0200

    This is a script that would help to generate random passwords

---
+++ Random_password.py
@@ -0,0 +1,14 @@
+import re
+import random
+
+
+def generate_random_word(length):
+    letters = 'a345bc678d79A0e0DfF456gHh9E0iDF34jk75l3RD45mYnTREoHJpVqrD123S42353Wstuvwxyz'
+    word = ''
+    for _ in range(length):
+        word += random.choice(letters)
+    return word
+
+random_word = generate_random_word(8)
+print(f"Random word: {random_word}")
+
